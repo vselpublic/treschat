173b45feae6cab1cb723ee5665f007e2468a002b
vsel <volodymyr.volodymyrovych@gmail.com>
2015-06-13 21:03:29 +0300

    ebextension with packages added

--- env.sh
+++ env.sh
@@ -2,6 +2,6 @@
 
 export FLASK_CONFIG="development"
 export SECRET_KEY="4bs^7bre5aq8%$&JFDgtfd4#@"
-export DEV_DATABASE_URL="postgresql://treschat_app:12345treschatdb67890@localhost/treschatdb"
+export DEV_DATABASE_URL="postgresql://treschat_app:12345treschatdb67890@fordevpurpose.cn2hbxgdnr5i.eu-west-1.rds.amazonaws.com:5432/treschatdb"
 export TEST_DATABASE_URL="postgresql://treschat_app:12345treschatdb67890@localhost/treschatdbtest"
 export PROD_DATABASE_URL="postgresql://treschat_app:12345treschatdb67890@localhost/treschatdb"
